c01c0a7ce4f7d59f654c73afe5ee55aafb7df774
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-11 08:14:28 +0530

    set...

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://sia-user-hx35.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 const port = process.env.PORT || 5000;
 
 app.use('/', client);
