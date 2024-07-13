5adb8daefeb574110f71730c113ed3dbf1194ee4
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-13 15:16:13 +0530

    for logn....h.

--- app.js
+++ app.js
@@ -11,25 +11,45 @@ require('./config/connection');
 
 // Load environment variables from .env file
 
-app.use(
-  cors({
-    origin: "*", // Your frontend URL
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-// process.env.CLIENT_ORIGIN
+const allowedOrigins = [
+  process.env.CLIENT_ORIGIN, // Your first frontend URL
+  process.env.CLIENT_ORIGIN_2  // Your second frontend URL
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Check if the incoming request's origin is in the allowed origins array
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_ORIGIN,, // Your frontend URL
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_ORIGIN,);
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.CLIENT_ORIGIN,);
+//   res.header('Access-Control-Allow-Methods', 'GET, POST');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 const port = process.env.PORT || 5000;
 
