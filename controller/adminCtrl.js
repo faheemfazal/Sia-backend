d2c4d05dcbf189c631af10a7bc401efd6c24c387
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-27 10:19:56 +0530

    format

--- controller/adminCtrl.js
+++ controller/adminCtrl.js
@@ -580,7 +580,7 @@ const postOpenorclose=async(req,res)=>{
 
         const result = await ShopDetail.updateOne(
             { name: 'admin' }, // Filter criteria
-            { $set: { openorclose: req.body.isONorOFF } }, // Update data
+            { $set: { openorclose: !req.body.isONorOFF } }, // Update data
             { upsert: true } // Create a new document if no document matches the filter
           );
         res.status(200).json({ message: `success`,openORclosed:result.openorclose });
