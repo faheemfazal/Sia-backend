3042896c55ca2ef48c8fdabb9c277539eb165907
faheemfazal <110841363+faheemfazal@users.noreply.github.com>
2026-05-15 20:24:09 +0530

    Update nodeMailer.js
    node mailer issue resolved

--- utilities/nodeMailer.js
+++ utilities/nodeMailer.js
@@ -10,13 +10,13 @@ const nodeMailer = (UserEmail) => {
         port: 465,
         secure: true,
         auth: {
-            user: 'prasad063918@gmail.com',
-            pass: 'kylh cmte jgdy luze',
+            user: process.env.Email,
+            pass: process.env.Password,
         },
     });
     console.log(UserOtp,'opopop',email);
     const mailOptions = {
-        from: 'prasad063918@gmail.com',
+        from: process.env.Email,
         to: email,
         subject: "Your OTP",
         text: `Hi, I am SIA-Ecommerce. Thank you for logging in. SIA is always with you.OTP: ${UserOtp}`
